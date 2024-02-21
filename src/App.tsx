e8a406ab708a0fd15b744fe2cc5811aa7c016dab
itkrivoshei <nikitakrivoshei@gmail.com>
2024-02-21 19:39:47 +0100

    Add App component

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='app bg-blue-500 text-white text-xl p-4'>Hello World</div>
+  );
+};
+
+export default App;
