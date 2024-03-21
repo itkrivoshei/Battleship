e135f5cff97379449ddc6069069f04c8a43bfe87
itkrivoshei <nikitakrivoshei@gmail.com>
2024-03-21 12:42:23 +0100

    Rething project

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App: React.FC = () => {
-  return (
-    <div className='app bg-blue-500 text-white text-xl p-4'>Hello World</div>
-  );
-};
-
-export default App;
