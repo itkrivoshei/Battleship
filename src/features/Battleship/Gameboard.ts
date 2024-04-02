198cee8c863484b38e47b01ca57f98d05594f18f
itkrivoshei <nikitakrivoshei@gmail.com>
2024-04-02 20:21:44 +0200

    Add Gameboard class to manage ships, attacks, and misses

---
+++ src/features/Battleship/Gameboard.ts
@@ -0,0 +1,70 @@
+import { type Ship, Orientation } from "./Ship";
+
+interface Coordinate {
+  x: number;
+  y: number;
+}
+
+export class Gameboard {
+  private ships: { ship: Ship; position: Coordinate }[] = [];
+  private attacks: Coordinate[] = [];
+  private misses: Coordinate[] = [];
+
+  placeShip(ship: Ship, position: Coordinate) {
+    this.ships.push({ ship, position });
+  }
+
+  receiveAttack(attackCoordinate: Coordinate) {
+    let hitFound = false;
+
+    for (const { ship, position } of this.ships) {
+      let shipOccupiesCoordinate = false;
+
+      if (ship.orientation === Orientation.Horizontal) {
+        const endPositionX = position.x + ship.length - 1;
+        shipOccupiesCoordinate =
+          attackCoordinate.y === position.y &&
+          attackCoordinate.x >= position.x &&
+          attackCoordinate.x <= endPositionX;
+      } else {
+        const endPositionY = position.y + ship.length - 1;
+        shipOccupiesCoordinate =
+          attackCoordinate.x === position.x &&
+          attackCoordinate.y >= position.y &&
+          attackCoordinate.y <= endPositionY;
+      }
+
+      if (shipOccupiesCoordinate) {
+        const hitPosition =
+          ship.orientation === Orientation.Horizontal
+            ? attackCoordinate.x - position.x
+            : attackCoordinate.y - position.y;
+        ship.hit(hitPosition);
+        hitFound = true;
+        break;
+      }
+    }
+
+    if (!hitFound) {
+      this.misses.push(attackCoordinate);
+    }
+    this.attacks.push(attackCoordinate);
+  }
+
+  hasShipAt(coordinate: Coordinate): boolean {
+    return this.ships.some(
+      ({ position }) =>
+        position.x === coordinate.x && position.y === coordinate.y,
+    );
+  }
+
+  isMissedAttack(coordinate: Coordinate): boolean {
+    return this.misses.some(
+      (miss) => miss.x === coordinate.x && miss.y === coordinate.y,
+    );
+  }
+
+  areAllShipsSunk(): boolean {
+    return this.ships.every(({ ship }) => ship.isSunk());
+  }
+}
